deb86550fba713e7531f871e087db80892909106
justin Kelley <justin.k.owen@gmail.com>
2021-01-20 01:02:14 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var friends = ["Emma", "Matt", "Blake", "Mark", "Noah"];
+var lines = "lines"
+var num = 99;
+
+function code() {
+    while (num > 0) { }
+    if (num > 0) { }
+    console.log(num + "lines of code in the file" + num + "lines of code",
+        friends[0] + "strikes one out, clears it all out" + (num - 1) + "lines of code in the file")
+}
+
+
+console.log(num + "lines of code in the file" + num + "lines of code",
+    friends[1] + "strikes one out, clears it all out" + (num - 1) + "lines of code in the file")
+
+
+console.log(num + "lines of code in the file" + num + "lines of code",
+    friends[2] + "strikes one out, clears it all out" + (num - 1) + "lines of code in the file")
+
+
+console.log(num + "lines of code in the file" + num + "lines of code",
+    friends[3] + "strikes one out, clears it all out" + (num - 1) + "lines of code in the file")
+
+
+console.log(num + "lines of code in the file" + num + "lines of code",
+    friends[4] + "strikes one out, clears it all out" + (num - 1) + "lines of code in the file")
+
+if (num === 0)
+    console.log(num + "lines of code in the file" + ".." + num + ".." + "lines of code" + friends[i] + ".." + "strikes one out, clears it all out" + (num === 0) + ".." + "lines of code in the file.")
+
